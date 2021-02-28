94cf3e781de4e1dd0f82becf3fa19d8c23800d55
kalnyzalan <kalny.zalan7@gmail.com>
2021-02-28 21:09:00 +0100

    Added GeoLocation record class

---
+++ src/main/java/GeoLocation.java
@@ -0,0 +1,8 @@
+package geolocator;
+/**
+ * Record class for wrapping geolocation information about an IP address.
+ */
+
+record GeoLocation(String as,String city,String country,String countryCode,String isp,double lat,double lon,
+        String org,String query,String region,String regionName,String timezone,String zip) {}
+
